dd01182917cba305bcd72f1733024faf43ba31a5
Abiel Zulio M <abielzm@gmail.com>
2022-01-12 09:15:28 +0700

    init windi + vercel adapter

---
+++ .vercel_build_output/static/_app/pages/__layout.svelte-39433f5c.js
@@ -0,0 +1,3 @@
+import{S as c,i as p,s as d,E as m,t as i,g as u,f,F as $,G as v,H as y,p as b,n as g,d as r}from"../chunks/vendor-7cb0d464.js";function S(a){let s,n,l;const o=a[1].default,e=m(o,a,a[0],null);return{c(){s=i("+ "),e&&e.c(),n=i(`
++`)},l(t){s=u(t,"+ "),e&&e.l(t),n=u(t,`
++`)},m(t,_){f(t,s,_),e&&e.m(t,_),f(t,n,_),l=!0},p(t,[_]){e&&e.p&&(!l||_&1)&&$(e,o,t,t[0],l?y(o,t[0],_,null):v(t[0]),null)},i(t){l||(b(e,t),l=!0)},o(t){g(e,t),l=!1},d(t){t&&r(s),e&&e.d(t),t&&r(n)}}}function h(a,s,n){let{$$slots:l={},$$scope:o}=s;return a.$$set=e=>{"$$scope"in e&&n(0,o=e.$$scope)},[o,l]}class k extends c{constructor(s){super();p(this,s,h,S,d,{})}}export{k as default};
---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import vercel from '@sveltejs/adapter-vercel';
+import { windi } from 'svelte-windicss-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: [
+		windi({
+			configPath: "windi.config.js"
+		})
+	],
+	kit: {
+		adapter: vercel(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;
